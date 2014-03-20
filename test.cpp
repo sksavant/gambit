5bdb2c5d4db78980edb87129f3a854fe16a84ced
Savant Krishna <savant.2020@gmail.com>
2014-03-21 00:01:58 +0530

    test stuff

--- test.cpp
+++ test.cpp
@@ -1,29 +1,55 @@
 #include "src/libgambit/libgambit.h"
 #include "boost/lexical_cast.hpp"
+#include <cstring>
 
 using boost::lexical_cast;
 
 int main(){
     Gambit::Rational y;
     int i=0;
-    std::string s = "1.573e";
+    std::string s = "1.573e2blahblah";
     std::cerr << "Test " << i++ << " " << s << "\n";
-    y = Gambit::lexical_cast<Gambit::Rational>(s);
+    try{
+        y = Gambit::lexical_cast<Gambit::Rational>(s);
+    }catch(Gambit::ValueException e){
+        std::cerr << "EXCEPTON\n";
+    }
     std::cerr << "MAIN " << y << "\n";
 
-    s = "1.573e2";
+    s = "1.573e";
     std::cerr << "Test " << i++ << " " << s << "\n";
-    y = Gambit::lexical_cast<Gambit::Rational>(s);
+    try{
+        y = Gambit::lexical_cast<Gambit::Rational>(s);
+    }catch(Gambit::ValueException e){
+        std::cerr << "EXCEPTON\n";
+    }
     std::cerr << "lexical " << y << "\n";
 
-    s = "1.573";
+    s = "1.573/";
     std::cerr << "Test " << i++ << " " << s << "\n";
-    y = Gambit::lexical_cast<Gambit::Rational>(s);
+    try{
+        y = Gambit::lexical_cast<Gambit::Rational>(s);
+    }catch(Gambit::ValueException e){
+        std::cerr << "EXCEPTON\n";
+    }
     std::cerr << "lexical " << y << "\n";
 
     s = "5/2";
     std::cerr << "Test " << i++ << " " << s << "\n";
-    y = Gambit::lexical_cast<Gambit::Rational>(s);
+    try{
+        y = Gambit::lexical_cast<Gambit::Rational>(s);
+    }catch(Gambit::ValueException e){
+        std::cerr << "EXCEPTON\n";
+    }
+    std::cerr << "lexical " << y << "\n";
+
+    s = "1.531e2";
+    std::cerr << "Test " << i++ << " " << s << "\n";
+    try{
+        y = Gambit::lexical_cast<Gambit::Rational>(s);
+    }catch(Gambit::ValueException e){
+        std::cerr << "EXCEPTON\n";
+    }
     std::cerr << "lexical " << y << "\n";
     return 0;
 }
